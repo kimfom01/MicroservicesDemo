a58c531f7b6948706a8be9f4f896cd4ea5d0b07d
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-28 21:49:36 +0300

    Initial commit

---
+++ frontend/PizzaInfo.cs
@@ -0,0 +1,12 @@
+namespace frontend;
+
+public class PizzaInfo
+{
+    public string PizzaName { get; set; }
+
+    public int Cost { get; set; }
+
+    public string Ingredients { get; set; }
+
+    public string InStock { get; set; }
+}
