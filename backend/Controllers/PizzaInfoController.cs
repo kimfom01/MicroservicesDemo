7588d8ddc2bd37d46a238c3d98aef65708707399
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-28 22:35:08 +0300

    Build docker image and docker-compose

--- backend/Controllers/PizzaInfoController.cs
+++ backend/Controllers/PizzaInfoController.cs
@@ -1,21 +1,15 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-using Microsoft.Extensions.Caching.Distributed;
-using System.Text.Json;
+namespace backend.Controllers;
 
-namespace backend.Controllers
+[ApiController]
+[Route("[controller]")]
+public class PizzaInfoController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class PizzaInfoController : ControllerBase
+    private static readonly PizzaInfo[] TheMenu = new[]
     {
-        private static readonly PizzaInfo[] TheMenu = new[]
-        {
             new PizzaInfo { PizzaName = "The Mighty Meatball", Ingredients = "Meatballs and cheese", Cost = 40, InStock = "yes"},
             new PizzaInfo { PizzaName = "Crab Apple", Ingredients = "Dungeness crab and apples", Cost = 35, InStock = "no"},
             new PizzaInfo { PizzaName = "Forest Floor", Ingredients = "Mushrooms, rutabagas, and walnuts", Cost = 20, InStock = "yes"},
@@ -24,17 +18,17 @@ namespace backend.Controllers
             new PizzaInfo { PizzaName = "Spice Coming At Ya", Ingredients = "Peppers, chili sauce, spicy andouille", Cost = 50, InStock = "yes"}
         };
 
-        private readonly ILogger<PizzaInfoController> _logger;
+    private readonly ILogger<PizzaInfoController> _logger;
 
-        public PizzaInfoController(ILogger<PizzaInfoController> logger)
-        {
-            _logger = logger;
-        }
+    public PizzaInfoController(ILogger<PizzaInfoController> logger)
+    {
+        _logger = logger;
+    }
 
-         [HttpGet]
-         public IEnumerable<PizzaInfo> Get()
-         {
-             return TheMenu;
-         }
+    [HttpGet]
+    public IEnumerable<PizzaInfo> Get()
+    {
+        return TheMenu;
+
     }
 }
